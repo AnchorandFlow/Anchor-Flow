69efcf103c5c06d718a68665b0fb22e165083130
Lindsey Border <lindsey.border@gmail.com>
2026-07-08 07:52:31 -0600

    fix: Health cards mobile layout

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260708-134331-f93456a";
+const CACHE_VERSION = "anchor-flow-v20260708-135223-877fc34";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
