049a07fea2c51c6bcbdbe4d96db52f9bb6d56784
Lindsey Border <lindsey.border@gmail.com>
2026-07-07 21:32:19 -0600

    fix coveData cross-device sync (Bug A)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260708-032331-92c53d9";
+const CACHE_VERSION = "anchor-flow-v20260708-033148-71a0a70";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
