b53384af18e6bbbe455a8971d0d136e294d0b305
Lindsey Border <lindsey.border@gmail.com>
2026-07-08 07:31:09 -0600

    feat: goal-reached confetti

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260708-043945-2e251d1";
+const CACHE_VERSION = "anchor-flow-v20260708-132351-887b757";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
