0a63d71eb467446adb54db1082e2617851af958b
Lindsey Border <lindsey.border@gmail.com>
2026-07-08 07:43:35 -0600

    feat: prize + packing confetti, Lighthouse icon

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260708-132351-887b757";
+const CACHE_VERSION = "anchor-flow-v20260708-134331-f93456a";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
