320cd69e86dc6ed05e68c8b6eb5d78611248b2ea
Lindsey Border <lindsey.border@gmail.com>
2026-07-09 08:10:35 -0600

    Today: unified glance card + real-data Top 3 + seaglass header + light-blue Compass

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260709-134934-24ca9b7";
+const CACHE_VERSION = "anchor-flow-v20260709-141016-bcc0923";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
