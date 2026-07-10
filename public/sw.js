fc21c918316c4d904deabf1633480a29bca8f65c
Lindsey Border <lindsey.border@gmail.com>
2026-07-09 21:43:16 -0600

    feat: countdowns on Today

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260710-033935-efd7b27";
+const CACHE_VERSION = "anchor-flow-v20260710-034235-edf2c3e";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
