527a4c5549585b93b4d9e12c8b9e4eae9c5a31d3
Lindsey Border <lindsey.border@gmail.com>
2026-07-11 19:02:31 -0600

    notification tap-to-open: data.url routing + af_dest boot shim

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260710-044848-0d9fcce";
+const CACHE_VERSION = "anchor-flow-v20260712-010229-a40891f";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
