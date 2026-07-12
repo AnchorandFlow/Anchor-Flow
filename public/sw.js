0949c01108cf0c06ecb51bdfdd4317e20f39a3d4
Lindsey Border <lindsey.border@gmail.com>
2026-07-12 08:04:43 -0600

    update banner tappable on vault screens

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260712-010229-a40891f";
+const CACHE_VERSION = "anchor-flow-v20260712-024022-cf3c88b";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
