4cc9fb9a903b10a6366894a47d94a9f57346b96f
Lindsey Border <lindsey.border@gmail.com>
2026-07-13 09:09:40 -0600

    F-16: pull no longer clobbers un-pushed dirty keys

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260712-024022-cf3c88b";
+const CACHE_VERSION = "anchor-flow-v20260713-150934-fa9e703";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
