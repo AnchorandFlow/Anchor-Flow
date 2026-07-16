1cea101e182498d30157cd3af4f6aabf937eeb0f
Lindsey Border <lindsey.border@gmail.com>
2026-07-15 22:49:04 -0600

    fix: password reset actually works (F-95 Critical)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260716-032438-70fd776";
+const CACHE_VERSION = "anchor-flow-v20260716-044813-41fb125";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
