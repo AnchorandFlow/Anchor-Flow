43ff4719912e2121f186150416d4d2a78a519a74
Lindsey Border <lindsey.border@gmail.com>
2026-07-21 10:33:21 -0600

    deploy: Lighthouse + onboarding wizard (both dark behind flags)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260720-044900-41149a1";
+const CACHE_VERSION = "anchor-flow-v20260721-163301-ec69d16";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
