9a9479637c3918491d0b3a051d0bdc36430de754
Lindsey Border <lindsey.border@gmail.com>
2026-07-27 15:36:43 -0600

    Travel: TravelOverviewCard on Flow home, fix af-open-vault deep-link, trip detail nav

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260727-205733-837bbec";
+const CACHE_VERSION = "anchor-flow-v20260727-213623-28cb47e";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
