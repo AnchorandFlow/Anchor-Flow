e1062ebc0176f8c7efddac110fef56fe2aa76c20
Lindsey Border <lindsey.border@gmail.com>
2026-07-27 22:05:46 -0600

    Batch: icon registry fixes, notification deep-links, Travel toggle cleanup

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260728-030452-7fa2991";
+const CACHE_VERSION = "anchor-flow-v20260728-040542-82597ae";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
