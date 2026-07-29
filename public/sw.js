efc64e7d1144eaca0dd3c7eff3b9d53cb468cc47
Lindsey Border <lindsey.border@gmail.com>
2026-07-28 21:18:55 -0600

    Fix: name propagation, Compass collapse, Sunset backdrop, Flow page reorder

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260728-040542-82597ae";
+const CACHE_VERSION = "anchor-flow-v20260729-031843-d092e48";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
