8441397968e08093938c8d3289dbd51be7911470
Lindsey Border <lindsey.border@gmail.com>
2026-07-29 21:10:38 -0600

    Recipes: URL import, remove old Recipe Import from Meal Bank

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260730-013720-f98d19c";
+const CACHE_VERSION = "anchor-flow-v20260730-031028-d88a01b";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
