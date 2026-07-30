e1315cad71b3dc0f88a6ad798fb8a499d0e410b3
Lindsey Border <lindsey.border@gmail.com>
2026-07-30 08:29:19 -0600

    Batch 3: retire dead BrainTab, rename Systems to Maintenance

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260730-140423-2faf1f8";
+const CACHE_VERSION = "anchor-flow-v20260730-142914-1808511";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
