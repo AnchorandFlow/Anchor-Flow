66e380bcda86cdf9b8009c431cc815db3399fdf2
Lindsey Border <lindsey.border@gmail.com>
2026-07-30 20:54:55 -0600

    Make Lighthouse default-on, remove af_lighthouse_v2 opt-in gate

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260730-163419-c80464c";
+const CACHE_VERSION = "anchor-flow-v20260731-025443-c7a851e";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
