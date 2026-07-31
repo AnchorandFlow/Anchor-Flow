617e19e82a0f3040246be1abbe90d8d8c6eb062a
Lindsey Border <lindsey.border@gmail.com>
2026-07-31 09:07:14 -0600

    Learning Phase 3: Plan tab with Today/Week/Year/Loops

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260731-144005-a01ed73";
+const CACHE_VERSION = "anchor-flow-v20260731-150707-758e1e2";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
