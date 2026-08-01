d653b04096c857b992b81a1ec08c717783fbfab4
Lindsey Border <lindsey.border@gmail.com>
2026-07-31 20:40:20 -0600

    Learning: collapsible sections, closed by default

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260801-005317-a3753aa";
+const CACHE_VERSION = "anchor-flow-v20260801-024017-43bdbef";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
