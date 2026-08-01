bbb84a443f45702f854b7a38d04c0fe922854dea
Lindsey Border <lindsey.border@gmail.com>
2026-07-31 22:45:20 -0600

    Fix: Compass chat JSON parse error, robust response parsing

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260801-043358-d4840e0";
+const CACHE_VERSION = "anchor-flow-v20260801-044518-08a2b6e";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
