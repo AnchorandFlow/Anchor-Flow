831c0a3c22e1d53c1823e32da1f9edfdaf5e10c6
Lindsey Border <lindsey.border@gmail.com>
2026-07-31 22:52:18 -0600

    Compass: render markdown in chat, tighten response length

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260801-044518-08a2b6e";
+const CACHE_VERSION = "anchor-flow-v20260801-045216-0d43b55";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
