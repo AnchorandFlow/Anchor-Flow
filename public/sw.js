c1663afc4896bb4cd1612485ce3a378106e82158
Lindsey Border <lindsey.border@gmail.com>
2026-08-03 15:43:44 -0600

    Waves: own nav entry replacing Weekly Rhythm, removed from Exhale tabs

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260803-202535-085cd9b";
+const CACHE_VERSION = "anchor-flow-v20260803-214324-9002549";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
