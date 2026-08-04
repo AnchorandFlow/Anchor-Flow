5329a270a8f647ab30df93924b2390317737dab8
Lindsey Border <lindsey.border@gmail.com>
2026-08-03 21:55:33 -0600

    Home: navy theme, back buttons, hide Maintenance + Inventory nav

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260804-032635-cc2365a";
+const CACHE_VERSION = "anchor-flow-v20260804-035527-0325775";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
