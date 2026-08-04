645a842997c19c8f0b377ec377a916d432fbba08
Lindsey Border <lindsey.border@gmail.com>
2026-08-04 12:13:15 -0600

    Fix: FlowHome dinner reads correct meals key, Cove sort mutation

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260804-173025-28ba1a4";
+const CACHE_VERSION = "anchor-flow-v20260804-181312-a6cbc07";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
