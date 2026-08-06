b196544b5d94655779f3bbd3b72fe6629776ac51
Lindsey Border <lindsey.border@gmail.com>
2026-08-05 22:51:29 -0600

    fix: restore Sunset header and close button; fix Ripples quote display and editing

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260806-044017-6ef37e4";
+const CACHE_VERSION = "anchor-flow-v20260806-045124-c42447c";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
