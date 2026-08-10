7929f85038140de0379b390255db80440425d758
Lindsey Border <lindsey.border@gmail.com>
2026-08-10 12:32:28 -0600

    feat: merge Field Trips into Beyond + expand Learning Resources to full library

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260810-140709-c6b9df0";
+const CACHE_VERSION = "anchor-flow-v20260810-183225-3c4790f";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
