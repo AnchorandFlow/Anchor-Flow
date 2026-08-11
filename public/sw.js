6c78e88b870089512249aecad1e1dca156e2bd2f
Lindsey Border <lindsey.border@gmail.com>
2026-08-11 12:36:06 -0600

    design: Celebrations — featured next-up panel + color-coded 2-column card grid

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260811-035824-b8a6879";
+const CACHE_VERSION = "anchor-flow-v20260811-183601-e0bbf3d";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
