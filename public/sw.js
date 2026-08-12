30f6dc9c20190fb7adcb0bae5aa6395a78bce1f4
Lindsey Border <lindsey.border@gmail.com>
2026-08-12 12:26:39 -0600

    feat: context-aware back links in vault sections (← People / ← Home / ← Anchor Home)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260812-174205-c445ebc";
+const CACHE_VERSION = "anchor-flow-v20260812-182634-46a8b3b";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
