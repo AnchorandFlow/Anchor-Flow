6f3947ef24ab4f5dd63b31573a1d56ae895e81dd
Lindsey Border <lindsey.border@gmail.com>
2026-08-12 12:40:18 -0600

    feat: add Horizon section with Travel, Celebrations, Safe Harbor accordions

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260812-182634-46a8b3b";
+const CACHE_VERSION = "anchor-flow-v20260812-184000-61b4b00";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
