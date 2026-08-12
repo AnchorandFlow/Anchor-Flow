809634ebaf2f4dc7ccdb8fada68b426437687d2e
Lindsey Border <lindsey.border@gmail.com>
2026-08-12 11:42:09 -0600

    feat: add People section with Health, Career, Pets accordions

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260812-171858-637f33c";
+const CACHE_VERSION = "anchor-flow-v20260812-174205-c445ebc";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
