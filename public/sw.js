cb02f7ece9798253477c9a6bdda085630a1f38fe
Lindsey Border <lindsey.border@gmail.com>
2026-08-13 08:06:53 -0600

    fix: remove Customize sidebar button from nav

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260813-042523-5c3434b";
+const CACHE_VERSION = "anchor-flow-v20260813-140650-18bac0f";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
