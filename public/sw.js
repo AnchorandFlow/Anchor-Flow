dabc76e03d61a146c02a0f36f6fb6d4d7c6f816d
Lindsey Border <lindsey.border@gmail.com>
2026-08-19 21:00:28 -0600

    fix: modal contrast, Ripples inputs, Cove share toggle, Cove list performance

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "anchor-flow-v20260819-054852-82e0505";
+const CACHE_VERSION = "anchor-flow-v20260820-030022-0421c5f";
 const STATIC_CACHE = CACHE_VERSION;
 
 // On install: do NOT skip waiting immediately.
